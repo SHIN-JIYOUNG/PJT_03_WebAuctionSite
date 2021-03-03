d74f30d468ce3e7a3d81fd143d1b1920c98aad21
SHIN-JIYOUNG <god.g0.shin@gmail.com>
2021-03-03 11:35:40 +0900

    Update 이용후기_기능구현_신지영.sql

--- DB_SQL/이용후기_기능구현_신지영.sql
+++ DB_SQL/이용후기_기능구현_신지영.sql
@@ -101,19 +101,20 @@ on at.mem_no=a.mem_no;
 
 
 
-/*실시간 후기 중, 하루동안 좋아요 높은 3개 후기 선노출*/
-select * from (
-select r.mem_no, m.mem_name, r.review_no, r.review_time, r.review_image, nvl(r.review_like,0), r.review_text, a.art_name, a.art_theme , a.art_tag1 , a.art_tag2 ,a.art_tag3, at.ARTIST_NAME
-from review_tb r
-inner join art_info_tb_test a
-on r.art_no=a.art_no
-inner join member_tb_test m
-on r.mem_no=m.mem_no
-inner join artist_info_tb_test at
-on at.mem_no=a.mem_no
-where TO_CHAR(r.review_time) = TO_CHAR(sysdate)
-order by r.review_like desc
-)
-where rownum<=3;
-/******왜 0을 넣었는데 desc에서 0이 맨 위로 갈까ㅠㅠㅠㅠㅠ? *****/
+ /*실시간 후기 중, 하루동안 좋아요 높은 3개 후기 선노출*/
+ select * from (
+ select r.mem_no, m.mem_name, r.review_no, r.review_time, r.review_image, nvl(r.review_like,0) review_like, r.review_text, a.art_name, a.art_theme , a.art_tag1 , a.art_tag2 ,a.art_tag3, at.ARTIST_NAME
+ from review_tb r
+ inner join art_info_tb_test a
+ on r.art_no=a.art_no
+ inner join member_tb_test m
+ on r.mem_no=m.mem_no
+ inner join artist_info_tb_test at
+ on at.mem_no=a.mem_no
+ where TO_CHAR(r.review_time) = TO_CHAR(sysdate)
+ order by review_like desc
+ )
+ where rownum<=3;
+ 
+ /*해결 완료!*/
 
